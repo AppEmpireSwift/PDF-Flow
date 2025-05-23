73930c7843212dc97764d6d8076ca61daafbdf56
Osman <HvoyaLoy@outlook.com>
2025-05-23 14:39:07 +0300

    Отчасти сверстал SettingsPage остаётся поиграть с цветами и поменять баннер

--- lib/app/ui/settings/settings.page.dart
+++ lib/app/ui/settings/settings.page.dart
@@ -1,3 +1,5 @@
+import 'package:PDF_Flow/style/color.style.dart';
+import 'package:PDF_Flow/style/style.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:PDF_Flow/app/ui/premium/widgets/premium_banner.dart';
@@ -16,72 +18,69 @@ class SettingsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leadingWidth: 70,
-        leading: WABackButton(),
-        title: Text('Settings'),
+        // leadingWidth: 70,
+        // leading: WABackButton(),
+        centerTitle: false,
+        title: Text('Settings', style: TextStyles.ForlargeTitleRegular),
       ),
-      body: ListView(
+      body: Container (
+        color: ColorStyles.Background,
+        child:  ListView(
         padding: EdgeInsets.all(16).r,
         children: [
-          SettingsSection(
-            children: [
-              SettingsTile(
-                title: 'Current version',
-                onTap: () => showAppVersion(context),
-              ),
-              Builder(
-                builder: (context) {
-                  return SettingsTile(
-                    title: 'Share ${Core.config.appName}',
-                    onTap: () {
-                      final box = context.findRenderObject() as RenderBox?;
-
-                      shareApp(box!.localToGlobal(Offset.zero) & box.size);
-                    },
-                  );
-                },
-              ),
-              SettingsTile(title: 'Rate App', onTap: openRateApp),
-            ],
-          ),
-          SizedBox(height: 8.r),
           AutoHiddablePremiumBanner(),
-          SizedBox(height: 8.r),
+          SizedBox(height: 28.r),
 
-          SettingsSection(
-            children: [
-              SettingsTile(
-                title: 'Resetting results',
+          Builder(
+            builder: (context) {
+              return SettingsTile(
+                title: 'Share ${Core.config.appName}',
                 onTap: () {
-                  //todo reset
-                },
-              ),
-            ],
-          ),
-          SizedBox(height: 16.r),
-          SettingsSection(
-            children: [SettingsTile(title: 'Contact us', onTap: contactUs)],
-          ),
-          SizedBox(height: 16.r),
-          SettingsSection(
-            children: [
-              SettingsTile(
-                title: 'App update',
-                onTap: () => checkAppUpdate(context),
-              ),
+                  final box = context.findRenderObject() as RenderBox?;
 
-              SettingsTile(
-                title: 'Privacy Policy',
-                onTap: () => openPrivacyPolicy(context),
-              ),
-              SettingsTile(
-                title: 'Terms of Use',
-                onTap: () => openTermsOfUse(context),
-              ),
-            ],
+                  shareApp(box!.localToGlobal(Offset.zero) & box.size);
+                },
+              );
+            },
           ),
+          SizedBox(height: 12.r),
+          SettingsTile(title: 'Rate us', onTap: openRateApp),
+          SizedBox(height: 32.r),
+
+
+          // SettingsSection(
+          //   children: [
+          //     SettingsTile(
+          //       title: 'Resetting results',
+          //       onTap: () {
+          //         //todo reset
+          //       },
+          //     ),
+          //   ],
+          // ),
+          // SettingsSection(
+          //   children: [SettingsTile(title: 'Contact us', onTap: contactUs)],
+          // ),
+
+          // SettingsTile(
+          //   title: 'App update',
+          //   onTap: () => checkAppUpdate(context),
+          // ),
+          SettingsTile(
+            title: 'Privacy Policy',
+            onTap: () => openPrivacyPolicy(context),
+          ),
+          SizedBox(height: 12.r),
+          SettingsTile(
+            title: 'Terms of Use',
+            onTap: () => openTermsOfUse(context),
+          ),
+          SizedBox(height: 12.r),
+
+          SettingsTile(title: 'Version', onTap: () => showAppVersion(context)),
         ],
       ),
+    ),
     );
   }
 }
--- lib/app/ui/settings/widgets/settings_tile.dart
+++ lib/app/ui/settings/widgets/settings_tile.dart
@@ -9,18 +9,38 @@ class SettingsTile extends StatelessWidget {
   final VoidCallback? onTap;
 
   const SettingsTile({super.key, required this.title, required this.onTap});
+
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      onTap: onTap,
-
-      contentPadding: EdgeInsets.symmetric(horizontal: 16).r,
-
-      title: Text(title),
-      titleTextStyle: TextStyles.bodyRegular.copyWith(
-        color: ColorStyles.primaryTxt,
+    return Padding( // Добавим отступ между плитками
+      padding: EdgeInsets.symmetric(vertical: 4.h),
+      child: Material(
+        color: ColorStyles.white,
+        borderRadius: BorderRadius.circular(8).r,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(8).r,
+          onTap: onTap,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.r, vertical: 16.h),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    title,
+                    style: TextStyles.bodyRegular.copyWith(
+                      color: ColorStyles.On_surface,
+                    ),
+                  ),
+                ),
+                Icon(
+                  CupertinoIcons.chevron_right,
+                  color: ColorStyles.Outline,
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
-      trailing: Icon(CupertinoIcons.chevron_right, color: ColorStyles.indigo),
     );
   }
 }
